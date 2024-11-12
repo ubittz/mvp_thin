66e9985d83a180de16c93c3c3074827bb1a2cd3a
uinus-wongil <uinus.kwk@uinus.co.kr>
2024-11-12 11:29:31 +0900

    feat: 수정 완료 hanlder 추가

--- src/pages/MyPage/Modify.tsx
+++ src/pages/MyPage/Modify.tsx
@@ -1,10 +1,13 @@
 import { Formik } from 'formik';
+import { useNavigate } from 'react-router-dom';
 
 import ModifyForm from '@@pages/MyPage/parts/ModifyForm';
 import { ModifyUserForm } from '@@pages/MyPage/type';
 import { useAppState } from '@@store/hooks';
 
 function Modify() {
+  const navigate = useNavigate();
+
   const me = useAppState((state) => state.home.me);
   const initialValues: ModifyUserForm = {
     id: 'th_in123',
@@ -15,8 +18,9 @@ function Modify() {
     phone: me?.phone ?? '',
   };
 
-  const handleSubmit = (form: ModifyUserForm) => {
-    console.log(form);
+  const handleSubmit = () => {
+    alert('수정이 완료되었습니다.');
+    navigate('/thin/my');
   };
 
   return (
